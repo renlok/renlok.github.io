b1b0c47a967d6a9179d30d2a4c0c9ed7ed93a475
Chris Dickenson <hopher10@googlemail.com>
2018-02-07 11:22:45 +0000

    Can not longer change routes while in trainer battle

--- src/actions.js
+++ src/actions.js
@@ -4,6 +4,10 @@ const UserActions = {
             dom.gameConsoleLog('It is too dangerous to travel without a pokemon.', 'red');
             return false;
         }
+        if (combatLoop.trainer) {
+            dom.gameConsoleLog('You cannot run away from a trainer battle.', 'red');
+            return false;
+        }
         player.settings.currentRouteId = newRouteId;
         if (ROUTES[player.settings.currentRegionId][player.settings.currentRouteId]['town']) {
             combatLoop.pause();
